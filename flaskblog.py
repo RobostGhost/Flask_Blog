05f2d68a3e3ff54f5f48b14406e6b877d7b86739
RobostGhost <robostghost@gmail.com>
2019-07-03 00:26:20 -0400

    static html templates now contained in html files, not inline

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,20 @@
-from flask import Flask
+
+from flask import Flask, render_template
+# Flask used to create app
+# render_template used to render an html file instead of inline html
+
 app = Flask(__name__)
 
 # Can have multiple routes for one function
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page<h1>"
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return "<h1>About Page<h1>"
+    return render_template("about.html")
+
 
 # Makes sure app runs in debug
 if __name__ == '__main__':
