5b469045024c5b3d2f1f477556480e3fe662b822
RobostGhost <robostghost@gmail.com>
2019-07-03 00:57:05 -0400

    Use if in template to determine title

--- flaskblog.py
+++ flaskblog.py
@@ -30,7 +30,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 
 # Makes sure app runs in debug
