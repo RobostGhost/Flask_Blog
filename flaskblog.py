afc2ada0758c6d3a5b95952e3b5b680b7f4295b5
RobostGhost <robostghost@gmail.com>
2019-07-03 00:48:25 -0400

    - added fake post data to render template - use html template for loop to render all post data

--- flaskblog.py
+++ flaskblog.py
@@ -5,11 +5,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# python dictionary representing post data
+posts = [
+    {
+        'author' :  'Mark Rob',
+        'title' :  'First Blog Post',
+        'content' :  'Hello World',
+        'date_posted' :  'July 3, 2019',
+    },
+    {
+        'author' :  'Apple Orange',
+        'title' :  'I am happy',
+        'content' :  'The title says it all',
+        'date_posted' :  'July 2, 2019',
+    }
+]
+
+
 # Can have multiple routes for one function
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
