e0d14fd04877b12be70de48f57444bcf06eddc52
RobostGhost <robostghost@gmail.com>
2019-07-04 19:50:57 -0400

    add flask-login which will be used to manage login sessions

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask # creates the app
 from flask_sqlalchemy import SQLAlchemy # create db
 from flask_bcrypt import Bcrypt # for hashing
+from flask_login import LoginManager # manages login sessions
 
 
 app = Flask(__name__)
@@ -12,10 +13,12 @@ app.config['SECRET_KEY'] = '7fec327b7e1da6f8b90966f78d7372d1'
 # /// indicates relative path, where the sb will be stored
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-# Getting the db
+# setup the db
 db = SQLAlchemy(app)
 # setup for password hasher
 bcrypt = Bcrypt(app)
+# setup login manager
+login_manager = LoginManager(app)
 
 # initialize routes
 from flaskblog import routes
