9b033276ad136d694fe268fae83c79356a91ffd3
RobostGhost <robostghost@gmail.com>
2019-07-04 03:14:01 -0400

    added db models to represent Users and Posts

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, render_template, url_for, flash, redirect
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 # Flask used to create app
@@ -19,6 +20,30 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 # Getting the db
 db = SQLAlchemy(app)
+
+# db Models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) #is the primary key of the db model
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg') # will use hash for image
+    password = db.Column(db.String(60), nullable=False) # hash of password
+    # One to Many relationship from User to Post
+    posts = db.relationship('Post', backref='author', lazy=True) # backref adds author attribute to Post
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) # date time function is passed, not run
+    content = db.Column(db.Text, nullable=False)
+    # foreign key to user table
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # hash of password
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 
 # python dictionary representing post data
