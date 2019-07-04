20266c2bf5e0c5c71a6b46b8fa619abf12315ac8
RobostGhost <robostghost@gmail.com>
2019-07-04 19:09:10 -0400

    add and initialize Bcrypt for hashing

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 #  Package structure, where app will initialize
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 # Flask used to create app
 # SQLAlchemy for database
 
@@ -16,6 +17,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 # Getting the db
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
 
 # initialize routes
 from flaskblog import routes
