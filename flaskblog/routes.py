ac648b0c07b15e23c841e09696cf2936aba3f37e
RobostGhost <robostghost@gmail.com>
2019-07-06 18:33:33 -0400

    update backend to save user picture and user in db to point to the new picture

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,3 +1,5 @@
+import secrets
+import os
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -84,11 +86,28 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
+def save_picture(form_picture):
+    random_hex_for_pic_name = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_f_name = random_hex_for_pic_name + f_ext
+
+    # app.root_path gives us the path to our app
+    picture_path = os.path.join(app.root_path, 'static/profile_pics/', picture_f_name)
+    form_picture.save(picture_path)
+
+    return picture_f_name
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        # profile pic update
+        if form.picture.data:
+            picture_f_name = save_picture(form.picture.data)
+            current_user.image_file = picture_f_name
+
         # user info is updated
         current_user.username = form.username.data
         current_user.email = form.email.data
