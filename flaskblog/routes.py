d756d588d1566d43f351706e3950f857fc7cf2e2
RobostGhost <robostghost@gmail.com>
2019-07-06 17:59:37 -0400

    user info always set in form initially

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -93,8 +93,10 @@ def account():
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit() # remember to actually save the changes to the db
-        flash('Account Updated!' 'successs')
-        
+        flash('Account Updated!', 'success')
         return redirect(url_for('account')) # ensures a GET once return to page
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
