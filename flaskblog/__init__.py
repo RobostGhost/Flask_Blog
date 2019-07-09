3ed1efbd387acd7747a510298a715cc026cf1680
RobostGhost <robostghost@gmail.com>
2019-07-09 16:40:21 -0400

    move app configuration to a config class

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,19 +1,15 @@
 #  Package structure, where app will initialize
-import os
 from flask import Flask # creates the app
 from flask_sqlalchemy import SQLAlchemy # create db
 from flask_bcrypt import Bcrypt # for hashing
 from flask_login import LoginManager # manages login sessions
 from flask_mail import Mail # for sending mail
+from flaskblog.config import Config
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-
-# used for protection (ex: cookie modification), token generated using secrets.token_hex()
-app.config['SECRET_KEY'] = '7fec327b7e1da6f8b90966f78d7372d1'
-# /// indicates relative path, where the sb will be stored
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 # setup the db
 db = SQLAlchemy(app)
@@ -23,13 +19,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login' # refers to our login page route func
 login_manager.login_message_category = 'info'
-# setup mail server
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = '587'
-app.config['MAIL_USE_TLS'] = True
-# Using user set environmental vars to ensure info not in code
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_EMAIL')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASS')
+
 mail = Mail(app)
 
 
