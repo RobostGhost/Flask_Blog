15211a6e105edbfed947e75e991b2adebb5e7000
RobostGhost <robostghost@gmail.com>
2019-07-10 14:42:07 -0400

    add routes which are used when a certain error happens

--- flaskblog/errors/handlers.py
+++ flaskblog/errors/handlers.py
@@ -1,3 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 errors = Blueprint('errors', __name__)
+
+
+# app_error handler for whole app, error_handler only for Blueprint specific
+
+@errors.app_errorhandler(404)
+def error_404(error):
+    # the number specifies the error to send, 200 by default
+    return render_template('errors/404.html'), 404
+
+
+@errors.app_errorhandler(403)
+def error_403(error):
+    return render_template('errors/403.html'), 403
+
+
+@errors.app_errorhandler(500)
+def error_500(error):
+    return render_template('errors/500.html'), 500
